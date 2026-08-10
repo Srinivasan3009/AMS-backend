6fd63133c30ead21c4c251c252160a211c81dae5
Srinivasan3009 <153206093+Srinivasan3009@users.noreply.github.com>
2026-08-10 22:40:32 +0530

    Email via sendgrid

--- utils/email.go
+++ utils/email.go
@@ -2,44 +2,85 @@ package utils
 
 import (
 	"fmt"
-	"net/smtp"
 	"os"
+
+	"github.com/sendgrid/sendgrid-go"
+	"github.com/sendgrid/sendgrid-go/helpers/mail"
 )
 
 // SendGradeNotification emails a student that their grade for a course has been posted/updated.
-func SendGradeNotification(toEmail, studentName, courseNo, courseName, term, grade string) error {
-	host := os.Getenv("SMTP_HOST")
-	port := os.Getenv("SMTP_PORT")
-	user := os.Getenv("SMTP_USER")
-	password := os.Getenv("SMTP_PASSWORD")
+func SendGradeNotification(
+	toEmail,
+	studentName,
+	courseNo,
+	courseName,
+	term,
+	grade string,
+) error {
+
+	apiKey := os.Getenv("SENDGRID_API_KEY")
+	fromEmail := os.Getenv("SMTP_USER")
 	fromName := os.Getenv("SMTP_FROM_NAME")
+
 	if fromName == "" {
 		fromName = "Anna University Portal"
 	}
 
-	if host == "" || port == "" || user == "" || password == "" {
-		return fmt.Errorf("SMTP is not configured")
+	if apiKey == "" {
+		return fmt.Errorf("SENDGRID_API_KEY is not configured")
 	}
 
-	subject := fmt.Sprintf("Grade Posted: %s (%s)", courseName, courseNo)
+	if fromEmail == "" {
+		return fmt.Errorf("SMTP_USER is not configured")
+	}
+
+	subject := fmt.Sprintf(
+		"Grade Posted: %s (%s)",
+		courseName,
+		courseNo,
+	)
+
 	body := fmt.Sprintf(
-		"Dear %s,\r\n\r\n"+
-			"Your grade for the following course has been posted:\r\n\r\n"+
-			"Course: %s - %s\r\n"+
-			"Term: %s\r\n"+
-			"Grade: %s\r\n\r\n"+
-			"You can view this in your student portal under Academic Information or Grade Viewer.\r\n\r\n"+
-			"Regards,\r\n%s",
-		studentName, courseNo, courseName, term, grade, fromName,
+		"Dear %s,\n\n"+
+			"Your grade for the following course has been posted:\n\n"+
+			"Course: %s - %s\n"+
+			"Term: %s\n"+
+			"Grade: %s\n\n"+
+			"You can view this in your student portal under Academic Information or Grade Viewer.\n\n"+
+			"Regards,\n%s",
+		studentName,
+		courseNo,
+		courseName,
+		term,
+		grade,
+		fromName,
 	)
 
-	msg := fmt.Sprintf(
-		"From: %s <%s>\r\nTo: %s\r\nSubject: %s\r\nMIME-Version: 1.0\r\nContent-Type: text/plain; charset=\"UTF-8\"\r\n\r\n%s",
-		fromName, user, toEmail, subject, body,
+	from := mail.NewEmail(fromName, fromEmail)
+	to := mail.NewEmail(studentName, toEmail)
+
+	message := mail.NewSingleEmail(
+		from,
+		subject,
+		to,
+		body,
 	)
 
-	auth := smtp.PlainAuth("", user, password, host)
-	addr := host + ":" + port
+	client := sendgrid.NewSendClient(apiKey)
 
-	return smtp.SendMail(addr, auth, user, []string{toEmail}, []byte(msg))
+	response, err := client.Send(message)
+
+	if err != nil {
+		return fmt.Errorf("SendGrid error: %w", err)
+	}
+
+	if response.StatusCode < 200 || response.StatusCode >= 300 {
+		return fmt.Errorf(
+			"SendGrid returned status %d: %s",
+			response.StatusCode,
+			response.Body,
+		)
+	}
+
+	return nil
 }
